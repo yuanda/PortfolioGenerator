b23ec1549fa3a256858677e8eb212fbf5b39ad45
yuanda <yuandahe1@gmail.com>
2016-02-08 22:52:55 -0500

    Add init gulp, bower, karma file.

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var gulp = require('gulp');
+var karma = require('karma').server;
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+
+gulp.task('test', function(done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done);
+});
+
+gulp.task('jshint', function() {
+  return gulp.src('src/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('serve', ['watch'], function() {
+  browserSync({
+    server: {
+      baseDir: './src'
+    }
+  });
+});
+
+gulp.task('sass', function() {
+  return gulp.src('src/styles/app.scss')
+    .pipe(sass())
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+    .pipe(gulp.dest('src/styles'));
+});
+
+gulp.task('reload', function() {
+  browserSync.reload();
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/styles/**/*.scss', ['sass']);
+  gulp.watch('src/**/*.html', ['reload']);
+  gulp.watch('src/**/*.js', ['reload']);
+});
+
+gulp.task('default',['serve']);
---
+++ test/sanity.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('Sanity',function () {
+  it('should work properly',function () {
+    expect(1).toBe(1);
+  });
+});
